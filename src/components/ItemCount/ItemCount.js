77ba3bf090e7cc8b0bfa1b929051b88aeec3fad9
Belen Korobka <belenkorobka@gmail.com>
2021-09-09 00:19:34 -0300

    Desafío: Contador con botón

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function ItemCount({ stock, initial}) {
+    const [count, setCount] = useState(initial)
+    const [message, setMessage] = useState('')
+
+    function add () {
+        if (count < stock) setCount((prevState) => prevState + 1);
+    }
+    function subtract () {
+        if (count > initial) setCount((prevState) => prevState - 1);
+    }
+
+    function addToCart() { 
+        setMessage(`Agregaste ${count} unidades`)  	
+    }
+
+    return (
+        <section className="itemCountContainer">
+            <div className="itemCountContainer__counter">
+                <button className="itemCountContainer__counter--subtract" onClick={subtract}></button>
+                <span className="itemCountContainer__counter--count">{count}</span>
+                <button className="itemCountContainer__counter--add" onClick={add}></button>
+            </div>
+            <div></div>
+            <button className="itemCountContainer__addButton" onClick={addToCart}>Añadir al carrito</button>
+            <p>{message}</p>
+        </section>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,10 @@
+import ItemCount from "../ItemCount/ItemCount"
+
 function ItemListContainer(props) {
     return (
         <section className="itemListContainer">
             <h1 className="itemListContainer__title">{props.greeting}</h1>
+            <ItemCount stock={12} initial={1}></ItemCount>
         </section>
     )
 }
